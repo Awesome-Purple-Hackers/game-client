2b86e06a0081f9d9b1fcae2ef301eeba99cf6197
rpuiu <raz.puiu@gmail.com>
2023-03-11 12:25:32 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:game_client/routes.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: Routes.initial,
+      getPages: Routes.routes,
+    );
+  }
+}
