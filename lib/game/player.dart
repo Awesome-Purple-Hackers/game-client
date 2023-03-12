9896bdf5def7fe8ae2f9e13ddb34fa2d058bdb4d
rpuiu <raz.puiu@gmail.com>
2023-03-12 09:59:37 +0200

    Home Screen with name: STABLE

--- lib/game/player.dart
+++ lib/game/player.dart
@@ -5,9 +5,16 @@ class Player extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(2.0),
-      child: Image.asset('lib/images/avatar.png'),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(11),
+      child: Container(
+        // color: Color.fromRGBO(133, 106, 30, 0.5),
+        color: Colors.orange,
+        child: Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: Image.asset('lib/images/avatar.png'),
+        ),
+      ),
     );
   }
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,45 +1,38 @@
-// import 'package:flutter/material.dart';
-// import 'package:game_client/routes.dart';
-// import 'package:get/get.dart';
-//
-// class HomeScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Stack(
-//         children: [
-//           // Background image
-//           Image.asset(
-//             'lib/images/splash.png',
-//             fit: BoxFit.cover,
-//             height: double.infinity,
-//             width: double.infinity,
-//           ),
-//           // Button and text container
-//           Positioned.fill(
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 SizedBox(height: 600),
-//                 ElevatedButton(
-//                   onPressed: () {
-//                     Get.toNamed(Routes.game);
-//                   },
-//                   child: Text('Button'),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:game_client/routes.dart';
 import 'package:get/get.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Offset> _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..repeat(reverse: true);
+    _animation = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(0.0, 0.2),
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,53 +44,80 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
         child: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Get.toNamed(Routes.game);
-            },
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurple),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                  side: BorderSide(color: Colors.deepPurple),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SlideTransition(
+                position: _animation,
+                child: Text(
+                  'WizQuiz',
+                  style: TextStyle(
+                    fontSize: 60.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                    // color: Color.fromRGBO(252, 2, 165, 1.0),
+                    color: Colors.white,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 2.0,
+                        // color: Colors.amberAccent.withOpacity(0.5),
+                        color: Colors.black54.withOpacity(1),
+                        offset: Offset(2.0, 2.0),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              elevation: MaterialStateProperty.all<double>(10.0),
-              overlayColor: MaterialStateProperty.resolveWith<Color>(
-                (Set<MaterialState> states) {
-                  if (states.contains(MaterialState.pressed)) {
-                    return Colors.deepPurple.withOpacity(0.8);
-                  }
-                  return Colors.deepPurple.withOpacity(0.2);
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  Get.toNamed(Routes.game);
                 },
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.play_arrow),
-                  SizedBox(width: 10),
-                  Text(
-                    "Start Game",
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1.2,
-                      shadows: [
-                        Shadow(
-                          blurRadius: 2.0,
-                          color: Colors.black.withOpacity(0.5),
-                          offset: Offset(1.0, 1.0),
-                        ),
-                      ],
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurple),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                      side: BorderSide(color: Colors.deepPurple),
                     ),
                   ),
-                ],
+                  elevation: MaterialStateProperty.all<double>(10.0),
+                  overlayColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.pressed)) {
+                        return Colors.deepPurple.withOpacity(0.8);
+                      }
+                      return Colors.deepPurple.withOpacity(0.2);
+                    },
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.play_arrow),
+                      SizedBox(width: 10),
+                      Text(
+                        "Start Game",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.2,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 2.0,
+                              color: Colors.black.withOpacity(0.5),
+                              offset: Offset(1.0, 1.0),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
         ),
       ),
